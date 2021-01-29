653546b8ae8826fd7cb7c65b03333f4d98cbb6b1
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-01-29 00:57:14 -0500

    the flipping works now, just gotta make it conditionally flip

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,30 +1,77 @@
-import React, { Component } from 'react'
-import LanguageApiService from '../../services/language-api-service'
-import '../../styles/card.css'
+import React, { Component } from "react";
+import LanguageApiService from "../../services/language-api-service";
+import "../../styles/card.css";
+
+function Front(props) {
+  return (
+    <div class="card front-card">
+      <div class="record">
+        <div class="right">{props.wordCorrectCount}</div>/
+        <div class="wrong">{props.wordIncorrectCount}</div>
+      </div>
+      <div class="word">{props.nextWord}</div>
+    </div>
+  );
+}
+
+function Back(props) {
+  return (
+    <div class="card back-card">
+      <div class="record">
+        <div class="right">{props.wordCorrectCount}</div>/
+        <div class="wrong">{props.wordIncorrectCount}</div>
+      </div>
+      <div class="word">{props.nextWord}</div>
+    </div>
+  );
+}
 
 class LearningRoute extends Component {
+  state = {
+    front: {},
+    guess: "",
+    back: {},
+  };
+
+  componentDidMount() {
+    LanguageApiService.getHead().then((head) => {
+      this.setState({
+        front: head,
+      });
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    LanguageApiService.postGuess(this.state.guess).then((results) => {
+      this.setState({
+        back: results,
+      });
+    });
+  }
+
   render() {
     return (
-      <section class="learning">
-            <div class="score">
-        Score: <div class="tally">195</div>
-    </div>
-    <div class="card">
-        <div class="record">
-            <div class="right">10</div>/<div class="wrong">7</div>
+      <section className="learning">
+        <div class="score">
+          Score: <div class="tally">{this.state.front.totalScore}</div>
         </div>
-        <div class="word">Weltschmertz</div>
-    </div>
-    <div class="answer-input">
-        <label for="answer">Answer:</label>
-        <input id="answer" />
-    </div>
-    <button class="next-button">
-        {"Next ->"}
-    </button>
+        <div className="flip">
+          <div class="flip-internal">
+            <Front {...this.state.front} />
+            <Back {...this.state.front} />
+          </div>
+        </div>
+        <div class="answer-input">
+          <label className="answer-label" for="answer">
+            Answer:
+          </label>
+          <input id="answer" />
+        </div>
+        <button class="next-button">{"Check"}</button>
       </section>
     );
   }
 }
 
-export default LearningRoute
+export default LearningRoute;

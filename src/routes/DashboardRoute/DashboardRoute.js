1e702a7c30f2eb37ce718a7b143c82617ef74194
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-01-30 03:47:00 -0500

    done styling, have not made responsive yet

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,51 +4,45 @@ import checkIcon from '../../resources/check-icon.png'
 import editIcon from '../../resources/edit-icon.png'
 import xIcon from '../../resources/x-icon.png'
 import LanguageApiService from '../../services/language-api-service'
+import {Link} from 'react-router-dom'
 
 function Word(word) {
   return  <div className="single-word">
   <div className="front"> {word.original} </div>
   <div className="back"> {word.translation} </div>
-  <img className="edit-icon" src={editIcon} />
-  <img className="x-icon" src={xIcon} />
+  <div class="correct-display"> &#10004;{word.correct_count}  </div>
+  <div class="incorrect-display"> &#10006;{word.incorrect_count} </div>
 </div>
 }
 
 class DashboardRoute extends Component {
 
   state = {
-    wordList: []
+  wordList: [], 
+  language: {}
   }
 
   componentDidMount() {
-    LanguageApiService.getHead().then(console.log)
-     LanguageApiService.getWords().then(wordList => {
-       this.setState({wordList: wordList.words})
-     })
+     LanguageApiService.getWords()
+     .then(languageWords => {
+       this.setState({wordList: languageWords.words, language: languageWords.language})
+      })
   }
 
 
 
 
   render() {
+    console.log(this.state.language)
     return (
-      <section>
-        <h1>German</h1>
-        <div className="progress-section">
-          10/17
-          <div className="progress">
-            <div className="progress-bar"></div>
-          </div>
+      <section className="dashboard">
+        <h1>{this.state.language.name}</h1>
+        <div className="score">
+          Score: <div class="tally">{this.state.language.total_score}</div>
         </div>
         <div className="wordlist">
-          <div className="single-word">
-            <input />
-            <input />
-            <img className="check-icon" src={checkIcon} />
-            <img className="x-icon" src={xIcon} />
-          </div>
           {this.state.wordList.map(Word)}
-          <button className="add-button"> + </button>
+          <Link to="/learn" className="learn-button"> Learn </Link>
         </div>
       </section>
     );
